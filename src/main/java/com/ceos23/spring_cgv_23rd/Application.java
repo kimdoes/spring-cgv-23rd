b7772fc48dd675ac5798c956d5e4ca9648b71b20
김도현 <kimdoes2143@naver.com>
2026-04-27 15:55:39 +0900

    change fatal errors

--- src/main/java/com/ceos23/spring_cgv_23rd/Application.java
+++ src/main/java/com/ceos23/spring_cgv_23rd/Application.java
@@ -14,9 +14,11 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.LocalDateTime;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableFeignClients
 public class Application {
--- src/main/java/com/ceos23/spring_cgv_23rd/Payment/Service/PaymentFacadeService/PaymentFacadeService.java
+++ src/main/java/com/ceos23/spring_cgv_23rd/Payment/Service/PaymentFacadeService/PaymentFacadeService.java
@@ -13,18 +13,6 @@ import org.springframework.web.server.ResponseStatusException;
 @Service
 public interface PaymentFacadeService {
     Payment buy(PaymentRequestDTO req, long targetId);
-    default boolean handleFeignException(FeignException fe){
-        fe.printStackTrace();
-
-        return switch (fe.status()) {
-            case 403 ->
-                    throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "결제 연동에 실패했습니다. (사유: 가맹점 storeId 불일치)");
-            case 404 ->
-                    throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "결제 연동에 실패했습니다. (사유: 존재하지 않는 가맹점)");
-            case 409, 500 -> true;
-            default -> throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, fe.getLocalizedMessage());
-        };
-    }
 }
 
 
